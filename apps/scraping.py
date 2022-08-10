12672d0c4484259f1efc20ddbb16973105151b39
yingko123 <yingko123@yahoo.com>
2022-08-10 13:01:06 -0500

    update

--- apps/scraping.py
+++ apps/scraping.py
@@ -12,6 +12,7 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=False)
 
     news_title, news_paragraph = mars_news(browser)
+    
 
     # Run all scraping functions and store results in dictionary
     data = {
